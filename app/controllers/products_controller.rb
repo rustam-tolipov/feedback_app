d3f782073b4cbddd48818d99e0039b812c28a1dd
rustam-tolipov <69011963+rustam-tolipov@users.noreply.github.com>
2025-06-17 15:40:00 +0500

    ✨ Display products with avg score and feedback count use fat model skinny controller approach

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,8 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product
+      .with_rating_data
+      .order('average_rating DESC')
+      .limit(20)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,16 @@
 class Product < ApplicationRecord
   has_many :feedbacks, dependent: :destroy
+
+  scope :with_rating_data, -> {
+    base = Product
+    .left_joins(:feedbacks)
+    .group('products.id')
+    .select(
+      'products.*',
+      'coalesce(avg(feedbacks.rating), 0) as average_rating',
+      'count(feedbacks.id) as feedback_count'
+      )
+
+      from(base, :products)
+  }
 end
